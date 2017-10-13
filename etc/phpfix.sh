036522d65d588567fa43d03bd365eca93a8d60b5
Robbie <txt3rob@gmail.com>
2017-10-13 09:47:32 +0100

    display PHP errors

--- etc/phpfix.sh
+++ etc/phpfix.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-upload_max_filesize = 900M
-post_max_size = 900M
+upload_max_filesize=900M
+post_max_size=900M
 max_execution_time=100
 max_input_time=223
 memory_limit=12048
+display_errors=on
 
-for key in upload_max_filesize post_max_size max_execution_time max_input_time memory_limit
+for key in upload_max_filesize post_max_size max_execution_time max_input_time memory_limit display_errors
 do
  sed -i "s/^\($key\).*/\1 $(eval echo \${$key})/" /etc/php5/apache2/php.ini
 done
